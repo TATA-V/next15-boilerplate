aeff2d63d832193c4ecf8f7639a41b2e2b909862
tatahyeonv <xhrl523@naver.com>
2024-11-22 23:10:45 +0900

    feat: add sign-in and sign-up pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublic = createRouteMatcher([
-  '/'
+  '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
 ]);
 
 export default clerkMiddleware(async(auth, request) => {
